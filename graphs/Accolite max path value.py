6d4ccc0bc5799caa3876e5576e7004a6be381d3b
Kartik Garg <kartikgargcs@gmail.com>
2021-05-15 12:18:33 +0530

    Update Accolite max path value.py

--- graphs/Accolite max path value.py
+++ graphs/Accolite max path value.py
@@ -4,7 +4,7 @@ def longestPath(nodes, edges):
     graph = collections.defaultdict(list)
     for i, j in edges:
         graph[i].append(j)
-
+    
     def cyclic(node, visited):
         if node not in visited:
             visited.add(node)
@@ -18,14 +18,29 @@ def longestPath(nodes, edges):
         value = [0] * 26
         for nei in graph[node]:
             value = [max(a, b) for a, b in zip(value, dfs(nei))]
-        value[ord(nodes[node]) - ord('A')] += 1
+        value[ord(nodes[node]) - ord('a')] += 1
+       
         return value
     
     return max(max(dfs(node)) for node in range(len(nodes)))
 
-nodes=["A","B","A","C","A"]
-edges=[(0, 1), (0, 2), (2, 3), (3, 4)]
-print(longestPath(nodes, edges))
+def beauty(n,m,s,x,y):
+    nodes=[i for i in s]
+    edges=[]
+    for j in range(m):
+        edges.append((x[j]-1,y[j]-1))
+    
+    ans=longestPath(nodes, edges)
+    if  ans==None:
+        ans=-1
+    return ans
+
+n=int(input())
+m=int(input())
+s=input()
+x=list(map(int,input().split()))
+y=list(map(int,input().split()))
+print(beauty(n,m,s,x,y))
 
 '''
 
